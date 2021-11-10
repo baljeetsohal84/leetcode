06a7cf7385e56b14b9ee89e81a2c537f992541b0
Baljeet Singh <baljeetsohal84@gmail.com>
2021-11-10 11:28:11 -0800

    Initial commit

---
+++ src/ReverseString.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "GeeksforGeeks";
+
+        byte[] strAsByteArray = input.getBytes();
+        byte[] result = new byte[strAsByteArray.length];
+
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Baljeet",1);
+        Employee e2 = new Employee("Avneet",2);
+
+        System.out.println(e1);
+        System.out.println(e2);
+    }
+}
